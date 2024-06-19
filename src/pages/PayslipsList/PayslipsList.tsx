20f63fbe14566a2f71f42fbdabc427b18ce67967
Daniel <d15_1_89@msn.com>
2024-06-19 15:06:15 +0200

    Loading skeleton

--- src/pages/PayslipsList/PayslipsList.tsx
+++ src/pages/PayslipsList/PayslipsList.tsx
@@ -9,23 +9,27 @@ import {
   IonHeader,
   IonPage,
   IonRow,
+  IonSkeletonText,
   IonTitle,
   IonToolbar,
   useIonLoading,
-  useIonViewDidEnter,
+  useIonViewWillEnter,
 } from '@ionic/react';
 import './PayslipsList.css';
 import { formatDate } from '../../utils/formatDate';
 import { setPayslips } from '../../store/payslipsSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../store';
+import { useState } from 'react';
 
 const PayslipsList: React.FC = () => {
   const dispatch = useDispatch();
   const payslips = useSelector((state: RootState) => state.payslips.payslips);
   const [present, dismiss] = useIonLoading();
+  const [loading, setLoading] = useState<boolean>(true);
 
-  useIonViewDidEnter(() => {
+  useIonViewWillEnter(() => {
+    setLoading(true);
     present('Getting payslips...');
     // This page content is just a mock so I'm faking a fetch with a loading modal
     setTimeout(() => {
@@ -51,7 +55,8 @@ const PayslipsList: React.FC = () => {
       ];
       dispatch(setPayslips(fetchedPayslips));
       dismiss();
-    }, 500);
+      setLoading(false);
+    }, 2000);
   });
 
   return (
@@ -64,22 +69,40 @@ const PayslipsList: React.FC = () => {
       <IonContent fullscreen>
         <IonGrid>
           <IonRow>
-            {payslips.map((payslip) => (
-              <IonCol size="6" size-md="3" size-lg="2" key={payslip.id}>
-                <IonCard
-                  className="payslip-card"
-                  routerLink={`/payslips/${payslip.id}`}
-                >
-                  <IonCardHeader>
-                    <IonCardTitle>{payslip.id}</IonCardTitle>
-                  </IonCardHeader>
-                  <IonCardContent>
-                    <p>From: {formatDate(payslip.fromDate)}</p>
-                    <p>To: {formatDate(payslip.toDate)}</p>
-                  </IonCardContent>{' '}
-                </IonCard>
-              </IonCol>
-            ))}
+            {loading
+              ? [...Array(10)].map((_, index) => (
+                  <IonCol size="6" size-md="3" size-lg="2" key={index}>
+                    <IonCard className="payslip-card">
+                      <IonCardHeader>
+                        <IonSkeletonText />
+                      </IonCardHeader>
+                      <IonCardContent>
+                        <p>
+                          <IonSkeletonText />
+                        </p>
+                        <p>
+                          <IonSkeletonText />
+                        </p>
+                      </IonCardContent>
+                    </IonCard>
+                  </IonCol>
+                ))
+              : payslips.map((payslip) => (
+                  <IonCol size="6" size-md="3" size-lg="2" key={payslip.id}>
+                    <IonCard
+                      className="payslip-card"
+                      routerLink={`/payslips/${payslip.id}`}
+                    >
+                      <IonCardHeader>
+                        <IonCardTitle>{payslip.id}</IonCardTitle>
+                      </IonCardHeader>
+                      <IonCardContent>
+                        <p>From: {formatDate(payslip.fromDate)}</p>
+                        <p>To: {formatDate(payslip.toDate)}</p>
+                      </IonCardContent>{' '}
+                    </IonCard>
+                  </IonCol>
+                ))}
           </IonRow>
         </IonGrid>
       </IonContent>
