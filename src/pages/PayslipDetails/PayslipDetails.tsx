f028a89432ab8fa3165a280664a9529405d5d6dd
Daniel <d15_1_89@msn.com>
2024-06-19 14:15:47 +0200

    Download native

--- src/pages/PayslipDetails/PayslipDetails.tsx
+++ src/pages/PayslipDetails/PayslipDetails.tsx
@@ -13,14 +13,21 @@ import {
   IonBackButton,
   IonFooter,
   IonButton,
+  useIonToast,
+  useIonLoading,
 } from '@ionic/react';
 import React from 'react';
 import { useParams } from 'react-router';
 import { calendar, documentText, downloadOutline } from 'ionicons/icons';
 import { formatDate } from '../../utils/formatDate';
+import { Capacitor } from '@capacitor/core';
+import { Directory, Filesystem } from '@capacitor/filesystem';
 
 const PayslipDetails: React.FC = () => {
   const { id } = useParams<{ id: string }>();
+  const [showToast] = useIonToast();
+  const isNative = Capacitor.isNativePlatform();
+  const [present, dismiss] = useIonLoading();
 
   // Mock data. In a real world, coming from API
   const myPayslip: Payslip = {
@@ -31,6 +38,55 @@ const PayslipDetails: React.FC = () => {
   };
 
   const handleDownload = () => {
+    isNative ? downloadNative() : downloadWeb();
+  };
+
+  const base64FromPath = async (path: string): Promise<Blob | string> => {
+    const response = await fetch(path);
+    const blob = await response.blob();
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        if (typeof reader.result === 'string') {
+          resolve(reader.result);
+        } else {
+          reject('method did not return a string');
+        }
+      };
+      reader.readAsDataURL(blob);
+    });
+  };
+
+  const downloadNative = async () => {
+    present('Downloading payslip...');
+    try {
+      const filename = `payslip_${myPayslip.id}.png`;
+      const base64Data = await base64FromPath(myPayslip.file);
+      await Filesystem.writeFile({
+        path: filename,
+        data: base64Data,
+        directory: Directory.Documents,
+      });
+
+      dismiss();
+      showToast({
+        message: 'Image downloaded. Check your gallery',
+        duration: 2000,
+        color: 'success',
+      });
+    } catch (error) {
+      dismiss();
+      showToast({
+        message: 'Something weird happened. Try later',
+        duration: 2000,
+        color: 'danger',
+      });
+      console.error(error);
+    }
+  };
+
+  const downloadWeb = () => {
     const link = document.createElement('a');
     link.href = myPayslip.file;
     link.setAttribute('download', `payslip_${myPayslip.id}.png`);
