29517ce326d83842812ed8cd2c6b0be7ab66b1f0
Daniel <d15_1_89@msn.com>
2024-06-19 11:14:47 +0200

    Invoice interface and grid

---
+++ src/interfaces/Invoice.ts
@@ -0,0 +1,6 @@
+interface Invoice {
+  id: number;
+  fromDate: string;
+  toDate: string;
+  file: string;
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,66 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import {
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonPage,
+  IonRow,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
 import './Home.css';
+
+const invoices: Invoice[] = [
+  {
+    id: 1,
+    fromDate: '2023-06-01',
+    toDate: '2023-06-30',
+    file: 'https://via.placeholder.com/100x200',
+  },
+  {
+    id: 2,
+    fromDate: '2023-07-01',
+    toDate: '2023-07-30',
+    file: 'https://via.placeholder.com/100x200',
+  },
+  {
+    id: 3,
+    fromDate: '2023-07-01',
+    toDate: '2023-07-30',
+    file: 'https://via.placeholder.com/100x200',
+  },
+];
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>My Invoices</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <IonGrid>
+          <IonRow>
+            {invoices.map((invoice) => (
+              <IonCol size="6" size-md="3" size-lg="2" key={invoice.id}>
+                <IonCard className="custom-card">
+                  <IonCardHeader>
+                    <IonCardTitle>Invoice {invoice.id}</IonCardTitle>
+                  </IonCardHeader>
+                  <IonCardContent>
+                    <p>From: {invoice.fromDate}</p>
+                    <p>To: {invoice.toDate}</p>
+                  </IonCardContent>{' '}
+                </IonCard>
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
